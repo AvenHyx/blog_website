0b30227f738d759bcbed6d86023548d7bea2724c
AvenHyx <974243307@qq.com>
2021-11-28 23:00:58 +0800

    add: antd pro 初始文件

---
+++ frontend/config/defaultSettings.js
@@ -0,0 +1,36 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#F5222D',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Ant Design Pro',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+  theme: "light",
+  hideCopyButton: false,
+  hideHintAlert: false
+  // "layout": "topmenu",
+  // "contentWidth": "Fixed",
+  // "theme": "light",
+  // "primaryColor": "#F5222D",
+  // "fixedHeader": false,
+  // "fixSiderbar": false,
+  // "hideHintAlert": false,
+  // "hideCopyButton": false
+  // {
+  //   "layout": "topmenu",
+  //   "contentWidth": "Fixed",
+  //   "theme": "light",
+  //   "primaryColor": "#F5222D",
+  //   "fixedHeader": false,
+  //   "fixSiderbar": false,
+  //   "hideHintAlert": false,
+  //   "hideCopyButton": false
+  // }
+};
+export default Settings;
